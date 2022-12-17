4a38ad92edd18055418f60d32ed3ea9b86dfea44
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-17 18:46:52 +0000

    Add a simple chat component

--- whatsapp-client/src/components/Sidebar/Sidebar.js
+++ whatsapp-client/src/components/Sidebar/Sidebar.js
@@ -1,12 +1,18 @@
+import Chat from "./components/Chat/Chat";
 import Navbar from "./components/Navbar/Navbar";
 import Search from "./components/Search/Search";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+	const chats = ["chat1", "chat2", "chat3"];
+
 	return (
 		<div className="sidebar__container">
 			<Navbar />
 			<Search />
+			{chats.map(() => (
+				<Chat />
+			))}
 		</div>
 	);
 };
---
+++ whatsapp-client/src/components/Sidebar/components/Chat/Chat.js
@@ -0,0 +1,11 @@
+import "./Chat.css";
+
+const Chat = () => {
+	return (
+		<div className="Chat">
+			<p>Chat goes here</p>
+		</div>
+	);
+};
+
+export default Chat;
