5dd0253514da4a458c9d98190c4c664823ee778d
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-20 21:45:05 +0000

    pass in props to chat component

--- whatsapp-client/src/components/Sidebar/Sidebar.js
+++ whatsapp-client/src/components/Sidebar/Sidebar.js
@@ -11,7 +11,11 @@ const Sidebar = () => {
 			<Navbar />
 			<Search />
 			{chats.map(() => (
-				<Chat />
+				<Chat
+					profileImageUrl="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+					chatTitle="Chat Title"
+					lastMessage="last message"
+				/>
 			))}
 		</div>
 	);
--- whatsapp-client/src/components/Sidebar/components/Chat/Chat.js
+++ whatsapp-client/src/components/Sidebar/components/Chat/Chat.js
@@ -1,17 +1,14 @@
 import "./Chat.css";
 
-const Chat = () => {
+const Chat = ({ profileImageUrl, chatTitle, lastMessage }) => {
 	return (
 		<div className="chat">
 			<div className="profile-picture-container">
-				<img
-					src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
-					className="profile-picture"
-				/>
+				<img src={profileImageUrl} className="profile-picture" />
 			</div>
 			<div className="chat-description">
-				<h3>chat title goes here</h3>
-				<p>last message goes here</p>
+				<h3>{chatTitle}</h3>
+				<p>{lastMessage}</p>
 			</div>
 		</div>
 	);
