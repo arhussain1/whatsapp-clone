f0782098a3168ff48d101ee323f1e5383db8afcf
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-20 21:06:12 +0000

    first pass for the chat design

--- whatsapp-client/src/components/Sidebar/components/Chat/Chat.js
+++ whatsapp-client/src/components/Sidebar/components/Chat/Chat.js
@@ -2,8 +2,17 @@ import "./Chat.css";
 
 const Chat = () => {
 	return (
-		<div className="Chat">
-			<p>Chat goes here</p>
+		<div className="chat">
+			<div className="profile-picture-container">
+				<img
+					src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+					className="profile-picture"
+				/>
+			</div>
+			<div className="chat-description">
+				<h3>chat title goes here</h3>
+				<p>last message goes here</p>
+			</div>
 		</div>
 	);
 };
