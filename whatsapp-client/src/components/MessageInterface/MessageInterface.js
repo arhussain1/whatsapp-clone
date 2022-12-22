f5137b494c8abd0c1483c027f6e6a7509b5f195b
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-22 18:56:58 +0000

    Add the styling for the chat messages

--- whatsapp-client/src/components/MessageInterface/MessageInterface.js
+++ whatsapp-client/src/components/MessageInterface/MessageInterface.js
@@ -1,11 +1,50 @@
 import "./MessageInterface.css";
 
-const MessageInterface = () => {
-	return (
-		<div className="MessageInterface__container">
-			<p>MessageInterface</p>
-		</div>
-	);
+const MessageInterface = (messages) => {
+	const owner = "Joker";
+
+	const tenMinsAgo = new Date(new Date().getTime() + -10 * 60000);
+	const fakeMessages = [
+		{
+			text: "Hey how are you",
+			owner: "Joker",
+			createdAt: tenMinsAgo,
+		},
+		{
+			text: "I'm good you",
+			owner: "Batman",
+			createdAt: new Date(),
+		},
+	];
+
+	const generateChatMessages = (chatMessages) => {
+		return chatMessages.map((message) => {
+			if (message.owner === owner) {
+				return (
+					<div className="messenger__message messenger__message-sent">
+						<span className="messenger__message-owner">{message.owner}</span>
+						<span>{message.text}</span>
+						<span className="messenger__message-timestamp">
+							{message.createdAt.toDateString()}
+						</span>
+					</div>
+				);
+			}
+			return (
+				<div className="messenger__message">
+					<span className="messenger__message-owner">{message.owner}</span>
+					<span>{message.text}</span>
+					<span className="messenger__message-timestamp">
+						{message.createdAt.toDateString()}
+					</span>
+				</div>
+			);
+		});
+	};
+
+	const generatedMessages = generateChatMessages(fakeMessages);
+
+	return <div className="MessageInterface__container">{generatedMessages}</div>;
 };
 
 export default MessageInterface;
