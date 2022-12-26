ad0dd81bc8e847f341b040a96c57a4588eda216f
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-26 20:32:53 +0000

    Return a JWT token when a user signs up

--- whatsapp-backend/controllers/users.js
+++ whatsapp-backend/controllers/users.js
@@ -1,4 +1,11 @@
 import User from "../models/user.js";
+import JsonWebToken from "jsonwebtoken";
+
+const createToken = (userId) => {
+	return JsonWebToken.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "2d",
+	});
+};
 
 const UsersController = {
 	Signup: async (req, res) => {
@@ -6,8 +13,8 @@ const UsersController = {
 
 		try {
 			const user = await User.signup(email, password, name);
-
-			res.status(200).json(user);
+			const jwtToken = createToken(user.id);
+			res.status(200).json(jwtToken);
 		} catch (error) {
 			res.status(400).json({ error: error.message });
 		}
