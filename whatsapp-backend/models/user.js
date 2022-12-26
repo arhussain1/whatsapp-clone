bcfce4a59c1784f9bcceda9a0e8e2e8a909f546e
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-26 17:25:55 +0000

    Add a signup function to handle validation and password hashing

--- whatsapp-backend/models/user.js
+++ whatsapp-backend/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
 	email: { type: String, required: true, unique: true },
@@ -6,6 +7,19 @@ const UserSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	chats: { type: Array, default: [] },
 });
+
+UserSchema.statics.signup = async function (email, password, name) {
+	const emailExists = await this.findOne({ email });
+
+	if (emailExists) throw Error("This email exists already");
+
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	const user = await this.create({ email, password: hashedPassword, name });
+
+	return user;
+};
 
 const User = mongoose.model("User", UserSchema);
 
