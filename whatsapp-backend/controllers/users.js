a335e1ceb784307a19cfb2ba61212188eb259ba6
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-26 17:59:48 +0000

    Add the signup helper method to the Signup controller

--- whatsapp-backend/controllers/users.js
+++ whatsapp-backend/controllers/users.js
@@ -1,6 +1,16 @@
+import User from "../models/user.js";
+
 const UsersController = {
 	Signup: async (req, res) => {
-		res.send("signup user");
+		const { email, password, name } = req.body;
+
+		try {
+			const user = User.signup(email, password, name);
+
+			res.status(200).json(user);
+		} catch (error) {
+			res.status(400).json({ error: error });
+		}
 	},
 };
 
