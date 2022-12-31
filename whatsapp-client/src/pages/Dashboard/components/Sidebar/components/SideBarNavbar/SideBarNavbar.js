c4d0567cc1faf1017c1c2ace334af12dabb99889
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-31 18:57:20 +0000

    Allow users to logout via the dropdown menu

--- whatsapp-client/src/pages/Dashboard/components/Sidebar/components/SideBarNavbar/SideBarNavbar.js
+++ whatsapp-client/src/pages/Dashboard/components/Sidebar/components/SideBarNavbar/SideBarNavbar.js
@@ -1,11 +1,23 @@
+import DropdownItem from "../../../../../../components/DotMenu/components/DropdownItem/DropdownItem";
 import DotMenu from "../../../../../../components/DotMenu/DotMenu";
+import useAuthContext from "../../../../../../hooks/useAuthContext";
 import "./SideBarNavbar.css";
 
 const SideBarNavbar = () => {
+	const { dispatch } = useAuthContext();
+
+	const logout = () => {
+		localStorage.removeItem("user");
+		dispatch({ type: "LOGOUT" });
+		console.log("I logged out");
+	};
+
 	return (
 		<div className="sidebar__navbar">
 			<p>Navbar goes here</p>
-			<DotMenu />
+			<DotMenu>
+				<DropdownItem title={"Logout"} action={logout} />
+			</DotMenu>
 		</div>
 	);
 };
