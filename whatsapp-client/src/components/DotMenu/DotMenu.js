41d70012575e3dae59a97c8b9549a0d608143b9b
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-31 19:06:47 +0000

    Close the dropdown menu when the user clicks outside of it

--- whatsapp-client/src/components/DotMenu/DotMenu.js
+++ whatsapp-client/src/components/DotMenu/DotMenu.js
@@ -1,12 +1,26 @@
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./DotMenu.css";
 
 const DotMenu = ({ children }) => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const dropdownRef = useRef(null);
+
+	useEffect(() => {
+		function handleClick(event) {
+			if (!dropdownRef.current.contains(event.target)) {
+				setIsMenuOpen(false);
+			}
+		}
+
+		document.addEventListener("click", handleClick);
+		return () => {
+			document.removeEventListener("click", handleClick);
+		};
+	}, []);
 
 	return (
-		<div className="dot-menu__container">
+		<div className="dot-menu__container" ref={dropdownRef}>
 			<div
 				className="dot__container"
 				onClick={() => {
