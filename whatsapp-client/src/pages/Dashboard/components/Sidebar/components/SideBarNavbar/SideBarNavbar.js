dc63dcc924d9342d13e6b062e54faa74690212bc
Abdul Hussain <a.r.hussain@hotmail.com>
2022-12-31 02:02:01 +0000

    Add the dot menu to the sidebar navbar

--- whatsapp-client/src/pages/Dashboard/components/Sidebar/components/SideBarNavbar/SideBarNavbar.js
+++ whatsapp-client/src/pages/Dashboard/components/Sidebar/components/SideBarNavbar/SideBarNavbar.js
@@ -1,9 +1,11 @@
+import DotMenu from "../../../../../../components/DotMenu/DotMenu";
 import "./SideBarNavbar.css";
 
 const SideBarNavbar = () => {
 	return (
 		<div className="sidebar__navbar">
 			<p>Navbar goes here</p>
+			<DotMenu />
 		</div>
 	);
 };
