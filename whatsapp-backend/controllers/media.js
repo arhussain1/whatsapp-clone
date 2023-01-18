34d13fded9cac3b32ea39b7a2a87ea338793e822
Abdul Hussain <a.r.hussain@hotmail.com>
2023-01-18 23:26:43 +0000

    upload the files directly to S3

--- whatsapp-backend/controllers/media.js
+++ whatsapp-backend/controllers/media.js
@@ -2,7 +2,7 @@ const MediaController = {
 	Index: async (req, res) => {
 		res.send("You called Media Index");
 	},
-	Create: async (req, res) => {
+	UploadImage: async (req, res) => {
 		console.log("file", req.file);
 		res.send("You called Media Create");
 	},
--- whatsapp-backend/routes/media.js
+++ whatsapp-backend/routes/media.js
@@ -1,13 +1,46 @@
 import express from "express";
 import MediaController from "../controllers/media.js";
 import multer from "multer";
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
+import multerS3 from "multer-s3";
+
+dotenv.config();
 
 const router = express.Router();
 
 // upload file to the servers file system using multer
 const localUpload = multer({ dest: "uploads/" });
 
+// get access to s3
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const S3 = new AWS.S3({
+	region,
+	accessKeyId,
+	secretAccessKey,
+});
+
+const uploadImage = multer({
+	storage: multerS3({
+		s3: S3,
+		bucket: bucketName,
+		metadata: (req, file, callback) => {
+			callback(null, { owner: "Abdul" });
+		},
+		key: (req, file, callback) => {
+			callback(
+				null,
+				"images/" + new Date().toISOString() + "-" + file.originalname
+			);
+		},
+	}),
+});
+
 router.get("/", MediaController.Index);
-router.post("/", localUpload.single("image"), MediaController.Create);
+router.post("/", uploadImage.single("image"), MediaController.UploadImage);
 
 export default router;
