966dd745e194ca6245d633997e62506a769eb664
Abdul Hussain <a.r.hussain@hotmail.com>
2023-02-08 13:18:58 +0000

    Stream the file back to the client

--- whatsapp-backend/controllers/media.js
+++ whatsapp-backend/controllers/media.js
@@ -1,6 +1,11 @@
+import { getFileStream } from "../s3.js";
+
 const MediaController = {
-	Index: async (req, res) => {
-		res.send("You called Media Index");
+	GetFile: async (req, res) => {
+		const key = req.params.key;
+
+		const readStream = getFileStream(key);
+		readStream.pipe(res);
 	},
 	UploadImage: async (req, res) => {
 		console.log("file", req.file);
