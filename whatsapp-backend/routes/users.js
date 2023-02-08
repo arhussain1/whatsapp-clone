43ab69411cd324dfbd17bc6ad5e1c554b7b05bac
Abdul Hussain <a.r.hussain@hotmail.com>
2023-02-08 13:22:57 +0000

    Add middleware to upload profile image to S3

--- whatsapp-backend/routes/users.js
+++ whatsapp-backend/routes/users.js
@@ -1,9 +1,14 @@
 import express from "express";
 import UsersController from "../controllers/users.js";
+import { uploadFile } from "../s3.js";
 
 const router = express.Router();
 
-router.post("/signup", UsersController.Signup);
+router.post(
+	"/signup",
+	uploadFile().single("profile-image"),
+	UsersController.Signup
+);
 router.post("/login", UsersController.Login);
 
 export default router;
